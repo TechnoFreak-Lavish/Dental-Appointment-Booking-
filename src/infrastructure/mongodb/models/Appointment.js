4a10063ec81e1001b057636ebc47ffb443b52401
RutviGhetiya1 <rutvighetiya4744@gmail.com>
2025-03-21 23:53:14 -0400

    Some changes in  Appoinments to connect the frontend properly.

--- src/infrastructure/mongodb/models/Appointment.js
+++ src/infrastructure/mongodb/models/Appointment.js
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
 
 const SchemaAppoinment = new mongoose.Schema({
-    patientID: {
-        type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true
-    },
-    Patientname: { type: String, required: true },
-    Patientemail: { type: String, required: true },
-    Patientphone: { type: String, required: true },
-    reason: { type: String, required: false },
-    date: {
-        type: Date, required: true
-    },
-    slot: {
-        type: String, required: true
-    },
-    appoinmentstatus: {
-        type: String, defafult: "Booked",
-        emun: ["Booked", "Cancelled", "Completed"]
-    },
-    clinic :{
-type :String
-    }
+  patientID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  reason: { type: String, required: false },
+  date: {
+    type: Date,
+    required: true,
+  },
+  slot: {
+    type: String,
+    required: true,
+  },
+  appoinmentstatus: {
+    type: String,
+    defafult: "Booked",
+    emun: ["Booked", "Cancelled", "Completed"],
+  },
+  clinic: {
+    type: String,
+  },
 });
 
 const Appointment = mongoose.model("Appoinment", SchemaAppoinment);
